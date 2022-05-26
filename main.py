e234d531bcc4d6d690c342154dbb7857595b46aa
niko006143 <r.r006143@gmail.com>
2022-05-26 12:00:43 +0300

    n

--- main.py
+++ main.py
@@ -45,6 +45,6 @@ if __name__ == '__main__':
             updater.bot.sendMessage(chat_id, text)
             path = existedProc[process]
             print( existedProc[process])
-            cmd = f"python3 {path}"
+            cmd = f"cd ..|python3 {path}"
             subprocess.Popen(cmd, shell=False)
         time.sleep(1)
