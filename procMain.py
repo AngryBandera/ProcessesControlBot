0bf36c820ddfa850480bfd251bca3bc071cff289
niko006143 <r.r006143@gmail.com>
2022-05-27 13:08:03 +0300

    fix

--- procMain.py
+++ procMain.py
@@ -1,14 +1,12 @@
 from telegram.ext import Updater
 import subprocess, time, sys, os
 
-target = input("Enter your processes, separated with /: ").split("/")
-
 targets = ""
 for t in target:targets+=t+" "
 
 print("Monitoring processes: " + targets)
 
-targetProcesses = {}
+targetProcesses = {"botsHandler": "opt/feedback/feedbot/SimpleFeedbackBot/botsHandler.py"}
 
 TOKEN = "5363787602:AAEkOHh1HFSXSWeL8wy5LH9cCKqrmSljpXA"
 chat_id = 729560932
@@ -17,23 +15,6 @@ updater = Updater(TOKEN, use_context=True)
 
 def getProcesses():
     return subprocess.getstatusoutput('ps -ef|grep python')[1].split("\n")
-
-def getPath(name):
-    path = subprocess.getstatusoutput(f'ps -ef | grep "{name}"'+"|grep -v grep| awk '{print $2}' | xargs pwdx")
-    return path
-
-for tg in target:
-    path = getPath(tg)
-    if path[0]!=0:
-        print(path[1])
-        continue
-
-    path = path[1].split("\n")[0][7:]
-
-    if "deleted" in path:
-        path = path[0:-10]
-
-    targetProcesses[tg] = path+f"/{tg}.py"
 
 print()
 if __name__ == '__main__':
@@ -48,9 +29,9 @@ if __name__ == '__main__':
 
         for process in existedProc.keys():
             updater = Updater(TOKEN, use_context=True)
+            path = existedProc[process]
             text = f"Process {path} is down. Restarting."
             updater.bot.sendMessage(chat_id, text)
             print(text)
-            path = existedProc[process]
             os.system(f"cd / && python3 {path}")
         time.sleep(1)
